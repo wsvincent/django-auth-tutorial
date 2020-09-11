7bebb43ec28f7f6016e6debbdb1fed75296161e8
Will Vincent <will@wsvincent.com>
2020-09-11 15:36:52 -0400

    3.1 updates

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from . import views
+from .views import SignUpView
 
 
 urlpatterns = [
-    path('signup/', views.SignUp.as_view(), name='signup'),
+    path('signup/', SignUpView.as_view(), name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views import generic
 
 
-class SignUp(generic.CreateView):
+class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
-    template_name = 'signup.html'
+    template_name = 'registration/signup.html'
