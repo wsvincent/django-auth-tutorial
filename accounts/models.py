8a22c2c84e85b1e99da5dd89f381c0d209cd9d35
Mohammadali <63012627+mohammadali-davarzani@users.noreply.github.com>
2021-03-29 01:12:02 -1200

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,15 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class User(AbstractUser):
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        UNSET = 'MF', 'Unset'
+
+    phone = models.CharField(max_length=15, blank=True)
+    address = models.TextField(blank=True)
+    gender = models.CharField(max_length=2, choices=Gender.choices, default=Gender.UNSET)
+    age = models.PositiveSmallIntegerField(blank=True, null=True)
+    description = models.TextField(blank=True)
