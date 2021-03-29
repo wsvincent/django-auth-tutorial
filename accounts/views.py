12f7e2df6372490df472fd98b1766a59401943c6
Mohammadali <63012627+mohammadali-davarzani@users.noreply.github.com>
2021-03-29 02:07:25 -1200

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,33 @@
-from django.contrib.auth.forms import UserCreationForm
-from django.urls import reverse_lazy
-from django.views import generic
+from rest_framework.generics import CreateAPIView
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.authtoken.views import obtain_auth_token
 
 
-class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('login')
-    template_name = 'registration/signup.html'
+from .serializers import UserSerializer
+
+Login = obtain_auth_token
+
+
+class LogoutAPIView(APIView):
+    pass
+
+
+class UserRegistration(CreateAPIView):
+    permission_classes = (AllowAny, )
+    serializer_class = (UserSerializer, )
+
+
+    def post(self, request):
+
+        user_serializer = UserSerializer(data=request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response({
+                "message" : "user create"
+            })
+
+
+        return Response({'message': user_serializer.errors})
